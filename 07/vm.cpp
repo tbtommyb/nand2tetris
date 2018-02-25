bf7e3a797a990b348719be968fa51f6d3f2add39
Tom Johnson <tom@tmjohnson.co.uk>
2018-02-25 10:45:19 +0000

    Handle directories

--- 07/vm.cpp
+++ 07/vm.cpp
@@ -1,33 +1,19 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include "boost/filesystem.hpp"
 #include "parser.hpp"
 #include "code_writer.hpp"
 
-std::string getName(std::string input)
+namespace fs = boost::filesystem;
+
+void process(fs::path input, fs::path output)
 {
-    std::istringstream iss{input};
-    if (std::getline(iss, input, '.')) {
-        return input;
-    }
-    return "";
-};
+    std::ifstream inputFile{input.string()};
+    std::ofstream outputFile{output.string(), std::ios_base::app};
 
-int main(int argc, char* argv[])
-{
-    if (argc != 2) {
-        std::cerr << "USAGE: vm input.vm" << std::endl;
-        exit(1);
-    }
-
-    auto filename = argv[1];
-    std::ifstream input{filename};
-
-    auto name = getName(filename);
-    std::ofstream out{name + ".asm"};
-
-    Parser parser{input};
-    CodeWriter writer{out, name};
+    Parser parser{inputFile};
+    CodeWriter writer{outputFile, input.stem().string()};
 
     while (parser.hasMoreCommands()) {
         parser.advance();
@@ -57,6 +43,29 @@ int main(int argc, char* argv[])
             writer.writeReturn(command);
             break;
         }
+    }
+};
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        std::cerr << "USAGE: vm input.vm" << std::endl;
+        exit(1);
+    }
+
+    fs::path input{argv[1]};
+
+    if (fs::is_directory(input)) {
+        for (const auto& entry : fs::directory_iterator(input)) {
+            const auto& file{entry.path()};
+
+            if (file.extension() == ".vm") {
+                std::cout << "processing: " << file.filename().string() << std::endl;
+                process(file, fs::path{input.parent_path().string() + ".asm"});
+            }
+        }
+    } else {
+        process(input, fs::path{input.stem().string() + ".asm"});
     }
 
     return 0;
